9239e5b5777b88cc68745aad1da871927baaca6a
Bryce McDonnell <bryce@bridgetownint.com>
2013-06-07 09:12:59 -0700

    better namespace, slight separation of concerns

--- table_definition/indexes.rb
+++ table_definition/indexes.rb
@@ -1,12 +1,17 @@
 require 'csv'
 
-module Indexes
-  module Indexes
-    def port_indexes(conf, db_name)
+module TableDefinition
+  class Indexes
+
+    def self.fetch_index_definition(conf, db_name)
       query = <<-SQL
         select * from information_schema.statistics WHERE TABLE_SCHEMA="#{db_name}"
       SQL
       command = "echo '#{query}' | mysql -h #{conf['host']} -u #{conf['username']} -p#{conf['password']} --batch --raw --default-character-set=utf8"
+    end
+
+    def self.create_index_statements(conf, db_name)
+      command = fetch_index_definition(conf, db_name)
       indexes = []
       index_string = ""
       CSV.parse(`#{command}`, :col_sep => "\t") do |row|
@@ -19,8 +24,11 @@ module Indexes
         seq_in_index = row[6].to_i
         column_name  = row[7]
         nullable     = row[12]
-        next unless table_name == 'accounts'
-        if seq_in_index == 1
+
+        if index_name == "PRIMARY"
+          indexes << "ALTER TABLE source_new.#{table_name} ADD PRIMARY KEY (#{column_name});"
+          index_string = ""
+        elsif seq_in_index == 1
           index_string << ");" unless index_string.empty?
           indexes << index_string unless index_string.empty?
           index_string = ""
@@ -29,7 +37,7 @@ module Indexes
           index_string << ", #{column_name}"
         end
       end
-      puts indexes.to_yaml
+      indexes
     end
   end
 end
