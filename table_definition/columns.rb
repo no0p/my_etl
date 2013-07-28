39b9051bbfff5320bf3ba87872ab04c465b3550a
robert berry <berrydigital@gmail.com>
2013-07-28 11:32:41 -0700

    allow for mirror schemas and replace flags

--- table_definition/columns.rb
+++ table_definition/columns.rb
@@ -23,12 +23,24 @@ module TableDefinition
         end unless (table == 'table_name' && column == 'column_name' && type == 'column_type')
       end
 
+      
         error_messages = []
         @schemas.each do |schema, tables|
           tables.each do |table, columns|
             destination_schema = ''
-            destination_schema = conf['destination_schema'] + '.' unless (conf['destination_schema'].empty? || conf['destination_schema'].nil?)
-            table_definition = "CREATE FOREIGN TABLE #{destination_schema}#{table} ("
+            destination_schema = conf['destination_schema'] + '.' unless (conf['destination_schema'].nil? || conf['destination_schema'].empty?)
+            
+            mirror_schema = ''
+            mirror_schema = conf[:mirror_schema] + '.' unless (conf[:mirror_schema].nil? || conf[:mirror_schema].empty?)
+            
+            table_definition = ""
+            table_definition += "DROP FOREIGN TABLE IF EXISTS #{destination_schema}#{table};" if conf[:replace]
+            
+            mirror_table_definition = ""
+            mirror_table_definition += "DROP TABLE IF EXISTS #{mirror_schema}#{table};" if conf[:replace]
+            
+            mirror_table_definition += "CREATE TABLE #{mirror_schema}#{table} ("
+            table_definition += "CREATE FOREIGN TABLE #{destination_schema}#{table} ("
             missed_columns = []
             columns.each do |c|
               col, type = c.to_a.flatten
@@ -47,13 +59,19 @@ module TableDefinition
                         end
               table_definition += "\"#{col}\" #{pg_type}" unless pg_type.nil?
               table_definition += ", " unless c == columns.last
+              
+              mirror_table_definition += "\"#{col}\" #{pg_type}" unless pg_type.nil?
+              mirror_table_definition += ", " unless c == columns.last
             end
             table_definition += ") "
+            mirror_table_definition += ") "
             table_definition += "SERVER #{conf['foreign_server']} "
             table_definition += "OPTIONS (table '#{schema}.#{table}');"
 
             if missed_columns.empty?
               all_tables << table_definition
+    
+              all_tables << mirror_table_definition unless conf[:mirror_schema].nil?
             else
               error_messages << "#{table} was not transferred because columns are missing: #{missed_columns.join(", ")}"
             end
